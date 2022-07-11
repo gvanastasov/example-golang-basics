32dd0dd8b2f9702e98bb70a31cb3fee4a87771b1
Georgi Anastasov <14071890+gvanastasov@users.noreply.github.com>
2022-07-11 12:58:37 +0200

    add typed send receive

--- channels/main.go
+++ channels/main.go
@@ -1,6 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+
+func worker(done chan bool) {
+	fmt.Print("working...")
+	time.Sleep(time.Second)
+	fmt.Println("done")
+
+	done <- true
+}
+
+func ping(pings chan<- string, msg string) {
+	pings <- msg
+}
+
+func pong(pings <-chan string, pongs chan<- string) {
+	msg := <-pings
+	pongs <- msg
+}
 
 func main() {
 	messages := make(chan string)
@@ -18,4 +38,15 @@ func main() {
 
 	fmt.Println(<-buffer)
 	fmt.Println(<-buffer)
+
+	done := make(chan bool, 1)
+	go worker(done)
+
+	<-done
+
+	pings := make(chan string, 1)
+	pongs := make(chan string, 1)
+	ping(pings, "passed message")
+	pong(pings, pongs)
+	fmt.Println(<-pongs)
 }
